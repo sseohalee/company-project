51d7e49e576f5bb627ef7b29510e5f0ac9f29b11
sseohalee <seohalee0131@gmail.com>
2024-04-22 08:59:53 +0900

    final

--- src/main/java/com/study/ProjectCompanyWeb/controller/CustomerController.java
+++ src/main/java/com/study/ProjectCompanyWeb/controller/CustomerController.java
@@ -37,9 +37,9 @@ public class CustomerController {
             customerService.qnaSave(reqDto);
         }catch (Exception e){
             e.printStackTrace();
-            return "<script>alert('묻고답하기 글 등록에 실패하였습니다.'); location.href='/';</script>";
+            return "<script>alert('묻고답하기 글 등록에 실패하였습니다.'); location.href='/customer/customer02';</script>";
         }
-        return "<script>alert('묻고답하기 글이 등록되었습니다.'); location.href='/';</script>";
+        return "<script>alert('묻고답하기 글이 등록되었습니다.'); location.href='/customer/customer02';</script>";
     }
 
     @GetMapping("/qna/pwCheck")
